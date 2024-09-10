79fb0fa2237420efced51b7eb0038425aee6063a
chaeminyu <chaeminyu>
2024-09-10 15:54:07 +0900

    [FEAT] Repository에 최근 등록 상품 중 Top10 반환 메서드

--- querydsl/src/main/java/study/querydsl/entity/Product.java
+++ querydsl/src/main/java/study/querydsl/entity/Product.java
@@ -1,12 +1,11 @@
 package study.querydsl.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -22,6 +21,14 @@ public class Product {
     private int price;
     private int stock;
     private int popularity;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+    @PrePersist
+    protected void onCreate(){
+        createdAt = LocalDateTime.now();
+    }
+
 
     public Product(String name, int price, int stock) {
         this.name = name;
