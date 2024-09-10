31f4e71a3ad0b0bbcab9e90d4a2fccf303acef47
chaeminyu <chaeminyu>
2024-09-10 15:45:43 +0900

    [FEAT] Repository에 인기 상품 Top10 반환 메서드

--- querydsl/src/main/java/study/querydsl/repository/ProductRepository.java
+++ querydsl/src/main/java/study/querydsl/repository/ProductRepository.java
@@ -1,7 +1,13 @@
 package study.querydsl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import study.querydsl.entity.Product;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    // 쿼리 메서드 - 인기상품 10위 반환
+    List<Product> findTop10ByOrderByPopularityDesc();
+
 }
