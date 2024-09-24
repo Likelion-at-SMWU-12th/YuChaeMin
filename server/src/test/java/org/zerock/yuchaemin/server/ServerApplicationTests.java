77f9afda7a9fc505a4000f3d1baf3ff1d2e99782
chaeminyu <chaeminyu>
2024-09-24 19:35:44 +0900

    [FEAT] server 1

---
+++ server/src/test/java/org/zerock/yuchaemin/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.yuchaemin.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
