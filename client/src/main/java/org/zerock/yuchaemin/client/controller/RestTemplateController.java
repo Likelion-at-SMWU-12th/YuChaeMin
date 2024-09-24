80075dd8316c73aa168be39ef12aeb1ccd082348
chaeminyu <chaeminyu>
2024-09-24 20:40:05 +0900

    [FEAT] Client Server 추가

--- client/src/main/java/org/zerock/yuchaemin/client/controller/RestTemplateController.java
+++ client/src/main/java/org/zerock/yuchaemin/client/controller/RestTemplateController.java
@@ -1,9 +1,14 @@
 package org.zerock.yuchaemin.client.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.yuchaemin.client.dto.MemberDto;
 import org.zerock.yuchaemin.client.service.RestTemplateService;
+
+import java.lang.reflect.Member;
 
 @RestController
 @RequestMapping("/rest-template")
@@ -17,4 +22,22 @@ public class RestTemplateController {
 
     @GetMapping
     public String getName() { return restTemplateService.getName(); }
+    @GetMapping("/path-variable")
+    public String getNameWithPathVariable() {
+        return restTemplateService.getNameWithPathVariable();
+    }
+    @GetMapping("/parameter")
+    public String getNameWithParameter() {
+        return restTemplateService.getNameWithParameter();
+    }
+
+    @PostMapping
+    public ResponseEntity<MemberDto> postDto() {
+        return restTemplateService.postWithParamAndBody();
+    }
+
+    @PostMapping("/header")
+    public ResponseEntity<MemberDto> postWithHeader() {
+        return restTemplateService.postWithHeader();
+    }
 }
